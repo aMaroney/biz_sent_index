e8736cd6b90f75d1748d749e71a3f311a251ddcd
alyssa <amaroney92@gmail.com>
2018-04-20 22:43:12 -0400

    v1

--- reddit_api_testing.py
+++ reddit_api_testing.py
@@ -5,20 +5,47 @@ import time
 import requests
 import re
 import sentiment_mod as sent
+import db_connection_secret as dbconnect
 from reddit_api_secret import *
 
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 # hot_python = subreddit.hot(limit = 4)
 
 def reddit_API_call(subreddit_name):
     subreddit = reddit.subreddit(subreddit_name)
-    # submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/88j45i/did_someone_lose_their_dog_found_in_wadeview_park/')
-    submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/8clikx/i_went_to_taco_maker_mexican_grill_a_new/')
+    submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/88j45i/did_someone_lose_their_dog_found_in_wadeview_park/')
+    # submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/8clikx/i_went_to_taco_maker_mexican_grill_a_new/')
     return submission
 
 
 submission = reddit_API_call('orlando')
-print(submission.score)
+
+def insert_text_into_table():
+    execute =("INSERT INTO biz_sent (reddit_text) VALUES (%s)")
+    submission_body = submission.selftext
+    submission_body_lower = submission_body.lower()
+    submission_body_processed = re.sub(r'([^\sa-z])+', '', submission_body_lower)
+    data = submission_body_processed
+    dbconnect.dbRemoteInsert(execute, data)
+    for comment in submission.comments.list():
+        comment_score = comment.score
+        comment_body = comment.body
+        comment_body_lower = comment_body.lower()
+        comment_body_processed = re.sub(r'([^\sa-z])+', '', comment_body_lower)
+        data = comment_body_lower
+        dbconnect.dbRemoteInsert(execute, data)
+
+insert_text_into_table()
+
+data_from_db = []
+
+def pull_text_from_table():
+    execute = 'SELECT reddit_text FROM biz_sent'
+    dbconnect.dbRemotePull(execute)
+
 dictionary = {}
 
 def write_post_to_keys():
@@ -34,7 +61,7 @@ def write_post_to_keys():
         no_special_char = re.sub(r'([^\sa-z])+', '', comment_body_lower)
         dictionary[no_special_char] = ''
 
-write_post_to_keys()
+# write_post_to_keys()
 
 def find_submission_sent():
     for key in dictionary.keys():
@@ -49,9 +76,7 @@ def find_submission_sent():
             print(e)
     return True
 
-find_submission_sent()
-
-print(dictionary)
+# find_submission_sent()
 
 def dbConnectionLocal(host, user, password, database):
     try:
@@ -59,77 +84,3 @@ def dbConnectionLocal(host, user, password, database):
         return connection
     except Exception as e:
                 print(e)
-
-def insertIntoTable():
-    connection = dbConnectionLocal('localhost', 'root', 'xxxx!', 'biz_index1')
-    cursor = connection.cursor()
-    subreddit_title = (subreddit.title)
-    submission_title = (submission.title)
-    submission_id = (submission.id)
-    submission_score = (submission.score)
-    submission_url = (submission.url)
-    submission_utc_created = (submission.created_utc)
-    for comment in submission.comments.list():
-        # print(20 * '-')
-        parent_id = str(comment.parent())
-        comment_id = comment.id
-        comment_score = comment.score
-        comment_body = comment.body
-        # print(dir(comment))
-        comment_time = comment.created_utc
-        comment_link = comment.permalink
-        unix_time = time.time()
-        cursor.execute(
-            "INSERT INTO biz_table1"
-            "("
-            "subreddit_title, "
-            "submission_title, "
-            "submission_id, "
-            "submission_score, "
-            "submission_url, "
-            "submission_utc_created,"
-            " comment_parent_id, "
-            "comment_comment_id, "
-            "comment_score, "
-            "comment_body, "
-            "comment_time, "
-            "comment_link"
-            ")"
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (subreddit_title,
-             submission_title,
-             submission_id,
-             submission_score,
-             submission_url,
-             submission_utc_created,
-             parent_id,
-             comment_id,
-             comment_score,
-             comment_body,
-             comment_time,
-             comment_link
-             ))
-    connection.commit()
-    connection.close()
-
-# Python code to remove duplicate elements
-def Remove(duplicate):
-    final_list = []
-    for num in duplicate:
-        if num not in final_list:
-            final_list.append(num)
-    return final_list
-
-def extractFromTable():
-    submission_id_list = []
-    connection = dbConnectionLocal('localhost', 'root', 'xxxx!', 'biz_index1')
-    cursor = connection.cursor()
-    cursor.execute('SELECT submission_title, comment_body FROM biz_table1')
-    submission_title = cursor.fetchall()
-    for row in submission_title:
-        print(row)
-    connection.commit()
-    connection.close()
-
-# insertIntoTable()
-# extractFromTable()
