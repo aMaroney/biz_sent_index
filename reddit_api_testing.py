a50d3345ca2185c6c86d976e6848a654c9130e61
alyssa <amaroney92@gmail.com>
2018-04-22 15:43:11 -0400

    v1

--- reddit_api_testing.py
+++ reddit_api_testing.py
@@ -1,12 +1,9 @@
-import praw
-import pymysql
 import sshtunnel
-import time
-import requests
 import re
-# import sentiment_mod as sent
+import sentiment_mod as sent
 import db_connection_secret as dbconnect
 from reddit_api_secret import *
+import pickle
 
 
 sshtunnel.SSH_TIMEOUT = 5.0
@@ -14,106 +11,87 @@ sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 # hot_python = subreddit.hot(limit = 4)
 
+dbconnect.truncateRemoteTable('biz_sent_reddit')
+print('cleared table: biz_sent_reddit')
+dbconnect.truncateRemoteTable('biz_sent_main')
+print('cleared table: biz_sent_main')
+
 def reddit_API_call(subreddit_name):
     subreddit = reddit.subreddit(subreddit_name)
-    submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/88j45i/did_someone_lose_their_dog_found_in_wadeview_park/')
+    # submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/88j45i/did_someone_lose_their_dog_found_in_wadeview_park/')
     # submission = reddit.submission(url='https://www.reddit.com/r/orlando/comments/8clikx/i_went_to_taco_maker_mexican_grill_a_new/')
+    submission = reddit.submission(
+        url='https://www.reddit.com/r/orlando/comments/88inz6/tell_me_switching_to_sprint_is_a_bad_idea/')
     return submission
 
 
 submission = reddit_API_call('orlando')
 
-#need to change so that everything from reddits api posts in the table
-def insert_text_into_table():
-    execute =("INSERT INTO biz_sent_text (reddit_text) VALUES (%s)")
+def insert_text_and_score_into_table():
+    execute =("INSERT INTO biz_sent_reddit (reddit_text, reddit_score) VALUES (%s, %s)")
     submission_body = submission.selftext
     submission_body_lower = submission_body.lower()
     submission_body_processed = re.sub(r'([^\sa-z])+', '', submission_body_lower)
-    data = submission_body_processed
+    reddit_score = submission.score
+    data = submission_body_processed, reddit_score
     dbconnect.dbRemoteInsert(execute, data)
     for comment in submission.comments.list():
         comment_score = comment.score
         comment_body = comment.body
         comment_body_lower = comment_body.lower()
         comment_body_processed = re.sub(r'([^\sa-z])+', '', comment_body_lower)
-        data = comment_body_lower
+        reddit_score = comment_score = comment.score
+        data = comment_body_lower, reddit_score
         dbconnect.dbRemoteInsert(execute, data)
 
-# insert_text_into_table()
+insert_text_and_score_into_table()
+print('inserted into reddit table')
 
-data_from_db = []
-
-#need to make this pull all the reddit info from table and write to a tuple
-def pull_text_from_table():
-    execute = 'SELECT reddit_text FROM biz_sent_text'
+def pull_text_and_score_from_table():
+    execute = 'SELECT reddit_text, reddit_score FROM biz_sent_reddit'
     table_text = dbconnect.dbRemotePull(execute)
     return table_text
-    # for i in range(len(table_text)):
-        # data_from_db.append(table_text[i])
 
-# dictionary = {}
-dictionary = {'': 'neg', 'you are a good man/woman!': 'pos', 'did you check at a vets to see if the dog has a chip?': 'neg', 'cutie': 'neg', 'update: took to pound and he is microchipped :) he\x92ll be reunited with his owner ': 'neg', 'i hope you find the owner :(': 'neg', 'aw, good! he\x92s a sweetie!': 'pos'}
+#dictionary[text] = sentiment_value
 
-#need to add sent to end of tuble for each text
-def find_submission_sent():
-    table_text = pull_text_from_table()
-    # print(table_text)
-    for tups in range(len(table_text)):
-        for text in table_text[tups]:
-            sentiment_value, confidence = sent.sentiment(text)
-            dictionary[text] = sentiment_value
-    return True
 
-# find_submission_sent()
+def find_sent():
+    tuper = ()
+    table_text = pull_text_and_score_from_table()
+    for i in range(len(table_text)):
+        for text in table_text:
+            try:
+                sentiment_value, confidence = sent.sentiment(text[i])
+                text = text + (sentiment_value, confidence)
+                tuper = tuper + (text,)
+            except Exception as e:
+                pass
+    return tuper
 
-#need to write final tuple to new table
-def write_everything_to_table():
+
+sent_result = find_sent()
+print('found the sentiment')
+
+pickle_out = open('testing.pickle', 'wb')
+pickle.dump(sent_result, pickle_out)
+pickle_out.close()
+print('pickled test')
+
+pickle_in = open('testing.pickle', 'rb')
+testing_file = pickle.load(pickle_in)
+pickle_in.close()
+
+def write_everything_to_main_table():
     # execute = ("INSERT INTO biz_sent_main (reddit_text, sentiment, confidence, reddit_score) VALUES (%s,%s,%s,%s)")
-    execute = ("INSERT INTO biz_sent_main (reddit_text, sentiment, reddit_score) VALUES (%s,%s,%s)")
+    execute = ("INSERT INTO biz_sent_main (reddit_text, sentiment, confidence, reddit_score) VALUES (%s,%s,%s,%s)")
     # for i in range(len(dictionary))
-    for key, value in dictionary.items():
-        submission_body_processed = key
-        sentiment = value
-        reddit_score = submission.comment.score
-        data = submission_body_processed, sentiment, reddit_score
+    for tup in range(len(testing_file)):
+        text = testing_file[tup][0]
+        reddit_score = testing_file[tup][1]
+        sent = testing_file[tup][2]
+        confidence = testing_file[tup][3]
+        data = text, sent, confidence, reddit_score
         dbconnect.dbRemoteInsert(execute, data)
 
-    # confidence =
-    # reddit_score =
-
-    # data = submission_body_processed, sentiment, confidence, reddit_score
-    # for comment in submission.comments.list():
-    #     comment_score = comment.score
-    #     comment_body = comment.body
-    #     comment_body_lower = comment_body.lower()
-    #     comment_body_processed = re.sub(r'([^\sa-z])+', '', comment_body_lower)
-    #     data = comment_body_lower
-    #     dbconnect.dbRemoteInsert(execute, data)
-
-write_everything_to_table()
-
-def write_post_to_keys():
-    submission_body = submission.selftext
-    submission_body_lower = submission_body.lower()
-    no_special_char = re.sub(r'([^\sa-z])+', '', submission_body_lower)
-    dictionary[no_special_char] = ''
-
-    for comment in submission.comments.list():
-        comment_score = comment.score
-        comment_body = comment.body
-        comment_body_lower = comment_body.lower()
-        no_special_char = re.sub(r'([^\sa-z])+', '', comment_body_lower)
-        dictionary[no_special_char] = ''
-
-# def find_submission_sent():
-#     for key in dictionary.keys():
-#         try:
-#             submission = key
-#             sentiment_value, confidence = sent.sentiment(submission)
-#             for key in dictionary.keys():
-#                 if key == submission:
-#                     dictionary[key] = sentiment_value
-#             # print(submission, sentiment_value, confidence)
-#         except Exception as e:
-#             print(e)
-#     return True
+write_everything_to_main_table()
+print('inserted everything into the main table')
