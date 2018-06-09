6c6ea4146b666ae3df293de9335f08a3b95d19a7
alyssa <amaroney92@gmail.com>
2018-06-09 17:53:46 -0400

    with reddit sent analyzer v.1

--- twitter_api_testing.py
+++ twitter_api_testing.py
@@ -1,8 +1,8 @@
 import tweepy
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-import json
+# from tweepy import Stream
+# from tweepy import OAuthHandler
+# from tweepy.streaming import StreamListener
+# import json
 import sentiment_mod as sent
 import db_connection_secret as dbconnect
 from twitter_api_secret import *
@@ -10,10 +10,13 @@ import re
 import time
 
 time_stamp = []
+# query = "\\""
+query = "pandas"
+number_of_results = 100
 
-def search_tweets(qurey, number_of_results):
+def search_tweets(query, number_of_results):
     api = tweepy.API(auth)
-    query = qurey
+    query = query
     max_tweets = number_of_results
     searched_tweets = []
     last_id = -1
@@ -25,29 +28,39 @@ def search_tweets(qurey, number_of_results):
                 break
             searched_tweets.extend(new_tweets)
             last_id = new_tweets[-1].id
+            # tweet = json.load(new_tweets)
+            # print(tweet)
         except tweepy.TweepError as e:
             # depending on TweepError.code, one may want to retry or wait
             # to keep things simple, we will give up on an error
             print(e)
+    tweepy_object_return = (searched_tweets[0])
+    # tweepy_return_as_json = json.dumps(tweepy_object_return._json)
     return searched_tweets
 
-def find_sent_of_tweets(qurey, number_of_results):
+def find_sent_of_tweets(query, number_of_results):
     lister_unfiltered = []
-    for tweets in search_tweets(qurey, number_of_results):
+    for tweets in search_tweets(query, number_of_results):
         tweet_text = tweets.text
+        follower_count = tweets.author.followers_count
+        screen_name = tweets.author.screen_name
+        tweet_id = tweets.author.id_str
+        tweet_created_at = str(tweets.created_at)
+        tweet_favorites = tweets.favorite_count
+        tweet_retweets = tweets.retweet_count
         submission_body_lower = tweet_text.lower()
         submission_body_processed = re.sub(r'([^\sa-z])+', '', submission_body_lower)
         # print(submission_body_processed)
         try:
             sentiment_value, confidence = sent.sentiment(submission_body_processed)
-            tuper_unfiltered = (tweet_text, sentiment_value, confidence)
+            tuper_unfiltered = (tweet_text, sentiment_value, confidence, follower_count, screen_name, tweet_id, tweet_created_at, tweet_favorites,tweet_retweets)
             lister_unfiltered.append(tuper_unfiltered)
-        except:
+        except Exception as e:
             pass
-
+            # print(e)
     return lister_unfiltered
 
-def calculate_query_sentiment():
+def calculate_query_sentiment(query, number_of_results):
     lister_filtered = []
     tweets_and_timestamps = []
     tweets_and_sent = []
@@ -56,8 +69,7 @@ def calculate_query_sentiment():
     tweets_and_timestamps.append(time_stamp[0])
     pos = 0
     neg = 0
-    query = "\"pee bottle\""
-    sent_of_tweets = find_sent_of_tweets(query, 100)
+    sent_of_tweets = find_sent_of_tweets(query, number_of_results)
     for result in sent_of_tweets:
         if result[1] == 'neg':
             # print(result)
@@ -97,58 +109,64 @@ def calculate_query_sentiment():
     # print(tweets_and_timestamps)
     # for i in tweets_and_timestamps:
     #     print(i)
-    return query, net_sentiment, tweets_and_timestamps, tweets_and_sent
-
+    return net_sentiment, tweets_and_timestamps, tweets_and_sent
+#
 dbconnect.truncateLocalTable('biz_sent_main')
 print('cleared table: biz_sent_main')
 
 def write_everything_to_main_table():
     # print('beginning write to database')
-    query, net_sentiment, main_table_write_return, tweets_and_sent = calculate_query_sentiment()
+    net_sentiment, main_table_write_return, tweets_and_sent = calculate_query_sentiment(query, number_of_results)
     execute = ("INSERT INTO "
                "biz_sent_main "
-               "(date_time_stamp, post_title, link_to_post, reddit_path_to_post, reddit_post_id, total_votes, net_sentiment) "
+               "(date_time_stamp, post_title, link_to_post, post_id, total_votes, net_sentiment, query_number) "
                "VALUES (%s,%s,%s,%s,%s,%s,%s)")
     time_stamp_key = time_stamp[0]
     # post_title = full_return[1]
     search_query = query
     link_to_post = ''
-    reddit_path_to_post = ''
     # reddit_post_id = full_return[4]
     twitter_post_id = ''
     # total_votes = full_return[5]
-    total_likes = 1
+    total_likes = None
+    query_number = number_of_results
     total_pos_votes = ''
     total_neg_votes = ''
     net_sentiment_twitter = net_sentiment
-    data = time_stamp_key,search_query,link_to_post,reddit_path_to_post,twitter_post_id,total_likes,net_sentiment
+    data = time_stamp_key,search_query,link_to_post,twitter_post_id,total_likes,net_sentiment, query_number
     dbconnect.dbLocalInsert(execute, data)
 
-write_everything_to_main_table()
+# write_everything_to_main_table()
 
 dbconnect.truncateLocalTable('biz_sent_twitter')
 print('cleared table: biz_sent_reddit')
 
 def write_everything_to_post_sent_table():
-    query, net_sentiment, main_table_write_return, tweets_and_sent = calculate_query_sentiment()
+    net_sentiment, main_table_write_return, tweets_and_sent = calculate_query_sentiment(query, number_of_results)
     time_stamp_key = time_stamp[0]
     query_from_search = query
-    # print(tweets_and_sent)
-    # print('beginning write to database')
     execute = ("INSERT INTO "
                "biz_sent_twitter"
-               "(time_stamp, query_from_search, twitter_text, twitter_confidence, twitter_sentiment)"
-               "VALUES (%s,%s,%s,%s,%s)")
-    for i in range(len(tweets_and_sent)):
-        # time_stamp_key = time_stamp[0]
-        tweet_sent_confidence = tweets_and_sent[i]
-        twitter_text = tweet_sent_confidence[0]
-        twitter_senitment = tweet_sent_confidence[1]
-        twitter_confidence = tweet_sent_confidence[2]
-        data = time_stamp_key, query_from_search, twitter_text, twitter_confidence, twitter_senitment
+               "(twitter_text, twitter_confidence, twitter_sentiment, time_stamp, query_from_search, follower_count, screen_name, tweet_id, tweet_created_at, tweet_favorites,"
+               "tweet_retweets)"
+               "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+    for tweet in tweets_and_sent:
+        twitter_text = tweet[0]
+        twitter_sentiment = tweet[1]
+        twitter_confidence = tweet[2]
+        follower_count = tweet[3]
+        screen_name = tweet[4]
+        tweet_id = tweet[5]
+        tweet_created_at = tweet[6]
+        tweet_favorites = tweet[7]
+        tweet_retweets = tweet[8]
+        data = twitter_text, twitter_confidence, twitter_sentiment, time_stamp_key, query_from_search, follower_count, screen_name, tweet_id, tweet_created_at, tweet_favorites, \
+               tweet_retweets
         dbconnect.dbLocalInsert(execute, data)
+    # # print(dir(tweepy_object_return.author))
 
-write_everything_to_post_sent_table()
+# write_everything_to_post_sent_table()
+
 
 # x = calculate_query_sentiment()
 
